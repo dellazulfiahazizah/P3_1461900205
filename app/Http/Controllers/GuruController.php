b33e7d7fe91671fb63ab77847d90d64c7ec549dd
dellazulfiahazizah <84627322+dellazulfiahazizah@users.noreply.github.com>
2021-06-09 21:26:42 +0700

    pertemuan 3

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -2,17 +2,112 @@
 
 namespace App\Http\Controllers;
 
-use App\Http|Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Guru;
 
-class GuruControllers\Controller{
-    public function show($id)
-}
-$guru = DB::table('')
-        ->select('.*')
-        ->get()
+class GuruController extends Controller
+{
+    public function index (Request $request)
+    {
+        $guru = Guru::when($request->cari, function ($query) use ($request) {
+            $query
+            ->where('nama', 'like', "%{$request->cari}%");
+        })->paginate(5);
 
-        return view('guru', [
-            'guru'=> $guru
+        $guru->appends($request->only('cari'));
+
+        return view('guru.index', [
+            'guru' => $guru,
+        ])
+        ->with('i', ($request->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('guru.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama'        => 'required',
+            'mengajar' => 'required',
         ]);
-        }
-    }        
+
+        Guru::create($request->all());
+
+        return redirect()
+                ->route('guru.index')
+                ->with('success','Buku created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Guru $guru)
+    {
+        return view('guru.show', compact('guru'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Guru $guru)
+    {
+        return view('guru.edit', compact('guru'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Guru $guru)
+    {
+        $request->validate([
+            'nama'        => 'required',
+            'mengajar' => 'required',
+        ]);
+
+        $guru->update($request->all());
+
+        return redirect()
+                ->route('guru.index')
+                ->with('success','Guru updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Guru $guru)
+    {
+        $guru->delete();
+
+        return redirect()->route('guru.index')
+                ->with('success','Guru deleted successfully');
+    }
+}
+
+    }
